1f73200f74c4e6e6db3c86e5e03ded617e579619
jester-lab86 <csaiyan@gmail.com>
2026-04-05 22:44:47 -0400

    fix: force unique build id every deploy

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  generateBuildId: async () => {
+    return Date.now().toString()
+  }
+};
 
 export default nextConfig;
